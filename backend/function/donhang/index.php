3ceae9ee78f324dd892051b99f0d5e3c92f1ee53
STUDENT <nhthuand20100@cusc.ctu.edu.vn>
2021-01-25 18:07:59 +0700

    Thêm mới đơn hàng

--- backend/function/donhang/index.php
+++ backend/function/donhang/index.php
@@ -5,22 +5,22 @@
 <html>
 
 <head>
-  <!-- Nhúng file quản lý phần HEAD -->
-  <?php include_once(__DIR__ . '/../../layouts/head.php'); ?>
-  <link rel="stylesheet" href="/Salomon/assets/vendor/DataTables/datatables.min.css" type="text/css">
-  <link rel="stylesheet" href="/Salomon/assets/vendor/DataTables/Buttons-1.6.5/css/buttons.bootstrap4.min.css" type="text/css">
+    <!-- Nhúng file quản lý phần HEAD -->
+    <?php include_once(__DIR__ . '/../../layouts/head.php'); ?>
+    <link rel="stylesheet" href="/Salomon/assets/vendor/DataTables/datatables.min.css" type="text/css">
+    <link rel="stylesheet" href="/Salomon/assets/vendor/DataTables/Buttons-1.6.5/css/buttons.bootstrap4.min.css" type="text/css">
 </head>
 
 <body class="d-flex flex-column h-100">
-  <!-- header -->
-  <?php include_once(__DIR__ . '/../../layouts/partials/header.php'); ?>
-  <!-- end header -->
+    <!-- header -->
+    <?php include_once(__DIR__ . '/../../layouts/partials/header.php'); ?>
+    <!-- end header -->
 
-  <div class="container-fluid">
+    <div class="container-fluid">
         <div class="row">
-                <!-- sidebar -->
-                <?php include_once(__DIR__ . '/../../layouts/partials/sidebar.php'); ?>
-                <!-- end sidebar -->
+            <!-- sidebar -->
+            <?php include_once(__DIR__ . '/../../layouts/partials/sidebar.php'); ?>
+            <!-- end sidebar -->
 
             <div class="col-md-9">
 
@@ -31,11 +31,11 @@
 
 
                 <?php
-                    // 1. Include file cấu hình kết nối đến database, khởi tạo kết nối $conn
-                    include_once(__DIR__ . '/../../../dbconnect.php');
+                // 1. Include file cấu hình kết nối đến database, khởi tạo kết nối $conn
+                include_once(__DIR__ . '/../../../dbconnect.php');
 
-                    // 2. Chuẩn bị câu truy vấn $sql
-                    $sql =  <<<EOT
+                // 2. Chuẩn bị câu truy vấn $sql
+                $sql =  <<<EOT
                     SELECT 
                         dh.dh_ma,dh.dh_ngaylap,dh.dh_ngaygiao,dh.dh_noigiao,dh.dh_trangthaithanhtoan,
                         httt.httt_ten,
@@ -48,23 +48,23 @@
                     GROUP BY dh.dh_ma             
 EOT;
 
-                    // 3. Thực thi câu truy vấn SQL để lấy về dữ liệu
-                    $result = mysqli_query($conn, $sql);
+                // 3. Thực thi câu truy vấn SQL để lấy về dữ liệu
+                $result = mysqli_query($conn, $sql);
 
 
-                    $ds_donhang = [];
-                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                        $ds_donhang[] = array(
-                            'dh_ma' => $row['dh_ma'],
-                            'dh_ngaylap' => date('d/m/Y H:i:s', strtotime($row['dh_ngaylap'])),
-                            'dh_ngaygiao' => date('d/m/Y H:i:s', strtotime($row['dh_ngaygiao'])),
-                            'dh_noigiao' => $row['dh_noigiao'],
-                            'dh_trangthaithanhtoan' => $row['dh_trangthaithanhtoan'],
-                            'httt_ten' => $row['httt_ten'],
-                            'kh_ten' => $row['kh_ten'],
-                            'TongThanhTien' => number_format($row['TongThanhTien'],"0",".",",").' VND'
-                        );
-                    }
+                $ds_donhang = [];
+                while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                    $ds_donhang[] = array(
+                        'dh_ma' => $row['dh_ma'],
+                        'dh_ngaylap' => date('d/m/Y H:i:s', strtotime($row['dh_ngaylap'])),
+                        'dh_ngaygiao' => date('d/m/Y H:i:s', strtotime($row['dh_ngaygiao'])),
+                        'dh_noigiao' => $row['dh_noigiao'],
+                        'dh_trangthaithanhtoan' => $row['dh_trangthaithanhtoan'],
+                        'httt_ten' => $row['httt_ten'],
+                        'kh_ten' => $row['kh_ten'],
+                        'TongThanhTien' => number_format($row['TongThanhTien'], "0", ".", ",") . ' VND'
+                    );
+                }
                 ?>
 
                 <table id="tblDH" width="100%" class="table table-bodered">
@@ -83,28 +83,28 @@ EOT;
                     </thead>
 
                     <tbody>
-                    <?php foreach ($ds_donhang as $dh) : ?>
-                        <tr>
-                            <td > <?= $dh['dh_ma'];?> </td>
-                            <td> <?= $dh['kh_ten']; ?> </td>
-                            <td> <?= $dh['dh_ngaylap']; ?> </td>
-                            <td> <?= $dh['dh_ngaygiao']; ?> </td>
-                            <td> <?= $dh['dh_noigiao']; ?> </td>
-                            <td> <span class="badge badge-primary"><?= $dh['httt_ten']; ?></span> </td>
-                            <td> 
-                                <?php if($dh['dh_trangthaithanhtoan'] == 0) : ?>
-                                    <span class="badge badge-danger">Chưa thanh toán</span>
-                                <?php else: ?>
-                                    <span class="badge badge-success">Đã thanh toán</span>
-                                <?php endif; ?>
-                            </td>
-                            <td style="text-align: right;"> <?= $dh['TongThanhTien']; ?> </td>
-                            <td>
-                                <a class="btn btn-primary" href="print.php?dh_ma=<?= $dh['dh_ma'];?>">In</a>
-                            </td>
-                            
-                        </tr>
-                    <?php endforeach; ?>
+                        <?php foreach ($ds_donhang as $dh) : ?>
+                            <tr>
+                                <td> <?= $dh['dh_ma']; ?> </td>
+                                <td> <?= $dh['kh_ten']; ?> </td>
+                                <td> <?= $dh['dh_ngaylap']; ?> </td>
+                                <td> <?= $dh['dh_ngaygiao']; ?> </td>
+                                <td> <?= $dh['dh_noigiao']; ?> </td>
+                                <td> <span class="badge badge-primary"><?= $dh['httt_ten']; ?></span> </td>
+                                <td>
+                                    <?php if ($dh['dh_trangthaithanhtoan'] == 0) : ?>
+                                        <span class="badge badge-danger">Chưa thanh toán</span>
+                                    <?php else : ?>
+                                        <span class="badge badge-success">Đã thanh toán</span>
+                                    <?php endif; ?>
+                                </td>
+                                <td style="text-align: right;"> <?= $dh['TongThanhTien']; ?> </td>
+                                <td>
+                                    <a class="btn btn-primary" href="print.php?dh_ma=<?= $dh['dh_ma']; ?>">In</a>
+                                </td>
+
+                            </tr>
+                        <?php endforeach; ?>
                     </tbody>
                 </table>
 
@@ -115,31 +115,31 @@ EOT;
             </div>
 
         </div>
-  </div>
+    </div>
 
-  <!-- footer -->
-  <?php include_once(__DIR__ . '/../../layouts/partials/footer.php'); ?>
-  <!-- end footer -->
+    <!-- footer -->
+    <?php include_once(__DIR__ . '/../../layouts/partials/footer.php'); ?>
+    <!-- end footer -->
 
-  <!-- Nhúng file quản lý phần SCRIPT JAVASCRIPT -->
-  <?php include_once(__DIR__ . '/../../layouts/scripts.php'); ?>
-  <script src="/Salomon/assets/vendor/DataTables/datatables.min.js"></script>
-  <script src="/Salomon/assets/vendor/DataTables/Buttons-1.6.5/js/buttons.bootstrap4.min.js"></script>
-  <script src="/Salomon/assets/vendor/DataTables/pdfmake-0.1.36/pdfmake.js"></script>
-  <script src="/Salomon/assets/vendor/DataTables/pdfmake-0.1.36/vfs_fonts.js"></script>
-  <script>
-    $(document).ready( function () {
-        $('#tblDH').DataTable({
-            dom: 'Blfrtip',
-            buttons: [
-            'copy', 'excel', 'pdf'
-            ]
+    <!-- Nhúng file quản lý phần SCRIPT JAVASCRIPT -->
+    <?php include_once(__DIR__ . '/../../layouts/scripts.php'); ?>
+    <script src="/Salomon/assets/vendor/DataTables/datatables.min.js"></script>
+    <script src="/Salomon/assets/vendor/DataTables/Buttons-1.6.5/js/buttons.bootstrap4.min.js"></script>
+    <script src="/Salomon/assets/vendor/DataTables/pdfmake-0.1.36/pdfmake.js"></script>
+    <script src="/Salomon/assets/vendor/DataTables/pdfmake-0.1.36/vfs_fonts.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#tblDH').DataTable({
+                dom: 'Blfrtip',
+                buttons: [
+                    'copy', 'excel', 'pdf'
+                ]
+            });
         });
-    } );
-  </script>
+    </script>
 
-  <!-- Các file Javascript sử dụng riêng cho trang này, liên kết tại đây -->
-  <!-- <script src="..."></script> -->
+    <!-- Các file Javascript sử dụng riêng cho trang này, liên kết tại đây -->
+    <!-- <script src="..."></script> -->
 </body>
 
 </html>
